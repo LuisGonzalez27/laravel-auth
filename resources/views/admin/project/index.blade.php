52d25be36fc588984b388ba75a24ed7177f0ece6
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-10 22:50:31 +0100

    created templates

---
+++ resources/views/admin/project/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{$project->name}}</li>
+        @endforeach
+    </ul>
+    
+@endsection
